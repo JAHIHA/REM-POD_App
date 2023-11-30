23c86ffd352ffb854aaa45df622ca0e02a7c914c
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2023-11-30 13:47:38 +0100

    ændringer

---
+++ REM POD_AppTests/files/ModelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REM_POD_App.files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REM_POD_App.files.Tests
+{
+    [TestClass()]
+    public class ModelTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
