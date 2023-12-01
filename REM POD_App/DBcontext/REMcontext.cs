3f8de8bc90ea40536878c71244eeaedcd6a4b7c6
Jakob Hillebrand Hanania <jako582n@edu.zealand.dk>
2023-12-01 10:27:48 +0100

    db context

---
+++ REM POD_App/DBcontext/REMcontext.cs
@@ -0,0 +1,17 @@
+﻿using REM_POD_App.files;
+using System.Collections.Generic;
+
+namespace REM_POD_App.DBcontext
+{
+    public class REMcontext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Secret.GetConnectionString);
+        }
+        public virtual DbSet<Model> model
+        {
+            get; set;
+        }
+    }
+}
